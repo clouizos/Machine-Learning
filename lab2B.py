9e9e42688f96d69afafb16c7fa92a5fea494d2bb
Christos Louizos <chr.louizos@gmail.com>
2013-09-19 18:25:36 +0200

    Coding seems to be finished for the 1st lab assignment. Now we need to answer the questions in 2.5 and write the report in Ipython Notebook.

--- lab2B.py
+++ lab2B.py
@@ -7,7 +7,6 @@ Created on Wed Sep 18 11:00:08 2013
 
 from __future__ import division
 import numpy as np
-import scipy as sp
 import matplotlib.pyplot as plt
 
 def gen_sinusoidal2(N):
@@ -16,14 +15,34 @@ def gen_sinusoidal2(N):
     mu, sigma = np.sin(x), 0.2
     t = np.random.normal(mu, sigma, N)
     return x,t
-    
-def getPhi(x, M):
+
+def smoothing(M, count):
+    #print x.shape[0]
+    #x = np.random.uniform(0,2*np.pi,count)
+    x = np.linspace(0, 2*np.pi,count, endpoint = True)
     phi = np.mat(np.zeros((x.shape[0],M+1)))
     for i in range(phi.shape[0]):
         for j in range(phi.shape[1]):
             if j==0:
                 phi[i,j] = 1
             phi[i,j] = np.power(x[i],j)
+    return phi,x
+    
+def getPhi(x, M):
+    try:
+        phi = np.mat(np.zeros((x.shape[0],M+1)))
+        for i in range(phi.shape[0]):
+            for j in range(phi.shape[1]):
+                if j==0:
+                    phi[i,j] = 1
+                phi[i,j] = np.power(x[i],j)
+    except: #scalar value
+        phi = np.zeros(M+1)
+        phi = phi.T
+        for i in range(phi.shape[0]):
+            if i==0:
+                phi[i] = 1
+            phi[i] = np.power(x, i)
     return phi
     
 def fit_polynomial_bayes(x, t, M, alpha, beta):
@@ -33,29 +52,61 @@ def fit_polynomial_bayes(x, t, M, alpha, beta):
     betaPhi = np.dot(np.dot(beta, phi.transpose()), phi)
     Sn = np.linalg.inv(aIden + betaPhi)
     mn = np.dot(np.dot(np.dot(beta, Sn), phi.transpose()), t)
-    return Sn, mn
+    mn = mn.T
+    #print mn.shape
+    #print Sn.shape
+    return mn, Sn
 
 def predict_polynomial_bayes(x, m, S, beta):
-    phi = getPhi(x, S.shape[0]-1)
-    predS = 1/beta + np.dot(np.dot(phi, S), phi.transpose())
-    predM = np.dot(m, phi.transpose())
-    return predS, predM
-    
-x,t = gen_sinusoidal2(7)
+    phi = getPhi(x, S.shape[0]-1).T
+    predS = 1/beta + np.dot(np.dot(phi.T, S), phi)
+    predM = np.dot(m.T, phi)
+    return predM, predS
+
+
+
+x, t = gen_sinusoidal2(7)
 alpha = 1/2
 beta = 1/(0.2)**2
 M = 5
-Sn, mn = fit_polynomial_bayes(x, t, M, alpha, beta)
-prediction_points = 10
-xforPred = np.linspace(0, 2*np.pi, prediction_points, endpoint = True)
-predS, predM = predict_polynomial_bayes(xforPred, mn, Sn, beta)
-phiPred = getPhi(xforPred, M+1)
-print predM.shape
-print phiPred.shape
-plt.figure("predictive distribution")
-plt.plot(xforPred, 1/(np.dot(predS, np.sqrt(2 * np.pi))) *
-             np.exp( - np.dot((phiPred - predM), 2) / np.dot(2, np.dot(predS, 2))),
-              linewidth=2, color='r')
-#plt.fill_between(,alpha = 0.1)
-plt.show()
+phi = getPhi(x, M)
+#print "phi shape:"+str(phi.shape)
+mn, Sn = fit_polynomial_bayes(x, t, M, alpha, beta)
+#print density
+#prediction_points = 1
+#xforPred = np.linspace(0, 2*np.pi, prediction_points, endpoint = True)
+xPred = np.linspace(0,2*np.pi,100)
+predM = np.zeros(len(xPred))
+predS = np.zeros(len(xPred))
+for i in range(len(xPred)):
+    predM[i], predS[i] = predict_polynomial_bayes(xPred[i], mn, Sn, beta)
+#print "predM"
+#print predM
+#print predS
+plt.figure(1)
+plt.subplot(211)
+plt.plot(x,t, 'co')
+plt.plot(xPred, predM,'green', label = 'mean')
+plt.plot(xPred, predM+np.sqrt(predS), 'r--', label = 'uncertainty')
+plt.plot(xPred, predM-np.sqrt(predS), 'r--')
+plt.fill_between(xPred,predM+np.sqrt(predS),predM-np.sqrt(predS),color = 'red',alpha=0.1)
+plt.xlim([0,2*np.pi])
+plt.legend()
 
+#plt.figure(2)
+plt.subplot(212)
+arrayM = np.squeeze(np.array(mn))
+wPost = np.random.multivariate_normal(arrayM,Sn, 100)
+for i in range(len(wPost)):
+    wPostSin = wPost[i]
+    #print wPostSin.shape
+    W = wPostSin.reshape(1,6)
+    smooth,xpol = smoothing(M, 100)
+    if i == 1:
+        plt.plot(xpol, np.dot(smooth, W.T),'r', label = 'polynomials')
+    else:
+        plt.plot(xpol, np.dot(smooth, W.T),'r')
+plt.xlim([0,2*np.pi])
+plt.legend()
+
+plt.show()
