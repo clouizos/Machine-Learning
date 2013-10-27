39747e5d4ebffe368fd053d3725a30813983e310
Christos Louizos <chr.louizos@gmail.com>
2013-10-27 11:28:24 +0100

    updated code for the last assignment

--- lab4.py
+++ lab4.py
@@ -8,6 +8,7 @@ Created on Wed Oct  9 08:20:16 2013
 from __future__ import division
 from operator import itemgetter
 from scipy import optimize
+from random import shuffle
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -34,7 +35,7 @@ t_test = add_noise( y_test, sigma )
 
 def k_n_m( xn, xm, thetas ):
     return thetas[0]*np.exp((-thetas[1]/2)*(xn - xm)**2)+thetas[2]+thetas[3]*xn*xm
-    
+        
 def computeK( X1, X2, thetas ):
     K = np.zeros((X1.shape[0],X2.shape[0]))
     #K = np.zeros((X1.shape[0],len(X2)))
@@ -95,7 +96,7 @@ def computek(x_train, x_test, theta):
     return k
     
     
-def gp_predictive_distribution( x_train, x_test, t_train, theta, C = None ):
+def gp_predictive_distribution( x_train, x_test, t_train, beta, theta, C = None ):
     if C == None:
         K = computeK(x_train, x_train, theta)
         C = computeC(K, beta)
@@ -129,7 +130,7 @@ def gp_plot( x_test, y_test, mu_test, var_test, x_train, t_train, theta, beta, l
     std_total = np.sqrt(np.diag(var_test))         # includes all uncertainty, model and target noise 
     std_model = np.sqrt( std_total**2 - 1.0/beta ) # remove data noise to get model uncertainty in stddev
     std_combo = std_model + np.sqrt( 1.0/beta )    # add stddev (note: not the same as full)
-    log_like = "%.2f" % log_like
+    #log_like = "%.2f" % log_like
     if label == None:
         plt.plot( x_test, y_test, 'b', lw=3, label = str(theta)+str(" ")+str(log_like))
     else:
@@ -165,19 +166,7 @@ def create_grid_thetas(max1,max2,max3,max4):
                     thetas.append((i,j,k,l))
     return thetas
 
-# function to optimize  
-def func_like(thetas):
-    thetas = np.exp(thetas)
-    return -gp_log_likelihood(x_train, t_train, thetas)
-
-# derivative of that function
-def func_like_prime(thetas):
-     return np.array((np.exp((-thetas[1]/2)*(np.linalg.norm(x)))),(),(),())
-
-# function that tries to learn the hyperparameters
-def learn_hyperparameters(nr_hyper):
-    print optimize.fmin_cg(func_like,0)
-    
+ 
     
 '''
 training for 2 datapoints
@@ -193,7 +182,7 @@ t_train = add_noise(y_train, sigma)
 
 plt.figure("predictive distribution with 2 training data points")
 for i in range(len(thetas)):
-    mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, thetas[i])
+    mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, beta, thetas[i])
     log_like = gp_log_likelihood(x_train, t_train, thetas[i])
     plt.subplot(2,3,i+1)
     gp_plot(x_test, y_test, mu_test, var_test, x_train, t_train, thetas[i], beta, log_like)
@@ -214,7 +203,7 @@ t_train = add_noise(y_train, sigma)
 
 plt.figure("predictive distribution with 10 training data points")
 for i in range(len(thetas)):
-    mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, thetas[i])
+    mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, beta, thetas[i])
     log_like = gp_log_likelihood(x_train, t_train, thetas[i])
     plt.subplot(2,3,i+1)
     gp_plot(x_test, y_test, mu_test, var_test, x_train, t_train, thetas[i], beta, log_like)
@@ -237,7 +226,7 @@ print "-------------------------------------------------------------------------
 
 #plot best combination of thetas
 plt.figure("best and worst thetas according to the grid search")
-mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, best)
+mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, beta, best)
 log_like = grid_res[0][1]
 plt.subplot(2,1,1)
 label = 'best'
@@ -247,7 +236,7 @@ plt.xlim(-1,1)
 #plt.ylim(-2,2)
 
 #plot worst combination of thetas
-mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, worst)
+mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, beta, worst)
 log_like = grid_res[len(grid_res)-1][1]
 plt.subplot(2,1,2)
 label = 'worst'
@@ -256,5 +245,104 @@ plt.legend(loc = 2, prop = {'size': 6})
 plt.xlim(-1,1)
 #plt.ylim(-2,2)
 
-learn_hyperparameters(4)
+def computeK_opt( X1, X2, phis ):
+    K = np.zeros((X1.shape[0],X2.shape[0]))
+    #K = np.zeros((X1.shape[0],len(X2)))
+    for i in range(len(X1)):
+        for j in range(len(X2)):            
+            K[i,j] = k_n_m_opt(X1[i],X2[j], phis)
+    return K
+    
+def k_n_m_opt(xn, xm, phis):
+    #return phis[0]*np.exp((-phis[1]/2)*(xn - xm)**2)+phis[2]+phis[3]*xn*xm
+    return np.exp(phis[0])*np.exp((-np.exp(phis[1])/2)*(xn - xm)**2)+np.exp(phis[2])+np.exp(phis[3])*xn*xm
+    #return np.exp(phis[0])*np.exp((-np.exp(phis[1])/2)*(xn - xm)**2)+phis[2]+np.exp(phis[3])*xn*xm
+ 
+# function to optimize       
+def gp_log_likelihood_opt(phis, *args):
+    x_train, t_train = args
+    #phis_ = np.zeros(4)
+    #phis_[0] = phis[0]
+    #phis_[1] = phis[1]
+    #phis_[2] = phi2
+    #phis_[3] = phi3
+    K = computeK_opt(x_train, x_train, phis)
+    #print K
+    C = computeC(K, beta)
+    invC = np.linalg.inv(C)
+    log_like = -(1/2)*np.log(np.linalg.det(C)) - (1/2)*np.dot(np.dot(t_train.T,invC),t_train) -(C.shape[0]/2)*np.log(2*np.pi)   
+    return -log_like
+
+# derivative of that function
+def grad_log_like(phis, *args):
+    a = 10**(-4)
+    #a = 10**(-5)
+    x_train, t_train = args
+    der = np.zeros_like(phis)
+    index_shuf = range(len(x_train))
+    index_shuf2 = range(len(x_train))
+    #shuffle(index_shuf)
+    #shuffle(index_shuf2)
+    K = computeK_opt(x_train, x_train, phis)
+    #print K
+    C = computeC(K, beta)
+    invC = np.linalg.inv(C)
+    for i in index_shuf:
+        for j in index_shuf2:
+            #dert0 = (np.exp((-np.exp(phis[1])/2)*(x_train[i] - x_train[j])**2))*np.exp(phis[0])
+            #dert1 = -0.5*np.exp(phis[0])*np.exp((-np.exp(phis[1])/2)*((x_train[i] - x_train[j])**2))*((x_train[i] - x_train[j])**2)*np.exp(phis[1])
+            #dert2 = np.exp(phis[2])
+            #dert3 = x_train[i]*x_train[j]*np.exp(phis[3])
+            
+            dert0 = np.exp((-np.exp(phis[1])/2)*(x_train[i] - x_train[j])**2)*np.exp(phis[0])
+            dert1 = -0.5*np.exp(phis[0])*np.exp((-np.exp(phis[1])/2)*((x_train[i] - x_train[j])**2))*((x_train[i] - x_train[j])**2)*np.exp(phis[1])
+            dert2 = np.exp(phis[2])
+            dert3 = x_train[i]*x_train[j]*np.exp(phis[3])
+            
+            der[0] = der[0] + a * np.exp(phis[0]) * (((-1/2)*np.trace(np.dot(invC, dert0))) + ((1/2)*np.dot(np.dot(np.dot(np.dot(t_train.T, invC),dert0), invC),t_train)))
+            der[1] = der[1] + a * np.exp(phis[1]) * (((-1/2)*np.trace(np.dot(invC, dert1))) + ((1/2)*np.dot(np.dot(np.dot(np.dot(t_train.T, invC),dert1), invC),t_train)))
+            der[2] = der[2] + a * np.exp(phis[2]) * (((-1/2)*np.trace(np.dot(invC, dert2))) + ((1/2)*np.dot(np.dot(np.dot(np.dot(t_train.T, invC),dert2), invC),t_train)))
+            der[3] = der[3] + a * np.exp(phis[3]) * (((-1/2)*np.trace(np.dot(invC, dert3))) + ((1/2)*np.dot(np.dot(np.dot(np.dot(t_train.T, invC),dert3), invC),t_train)))
+    return der
+   
+args = (x_train, t_train)
+#args = (x_train, t_train, 0, 0)
+opts = {'maxiter' : None,    # default value.
+        'disp' : True,    # non-default value.
+        'gtol' : 1e-5,    # default value.
+        'norm' : np.inf,  # default value.
+        'eps' : 1.4901161193847656e-08}  # default value.
+        
+init_phis = np.asarray((2, 5, 0, 0)) 
+#init_phis = np.asarray((0, 0)) 
+
+args2 = (x_train, t_train)
+err = optimize.check_grad(gp_log_likelihood_opt, grad_log_like, init_phis, x_train, t_train)
+print "error: " + str(err)
+print
+
+result = optimize.fmin_cg(gp_log_likelihood_opt, init_phis, args=args)
+#result = optimize.fmin_cg(gp_log_likelihood_opt, init_phis, fprime=grad_log_like, args=args)
+#result = optimize.fmin_bfgs(gp_log_likelihood_opt, init_phis, args=args)
+#result = optimize.minimize(gp_log_likelihood_opt, init_phis, jac = grad_log_like, args = args, method = 'CG', options = opts)
+#result = optimize.minimize(gp_log_likelihood_opt, init_phis, args = args, method = 'CG', options = opts)
+print 'result'
+#print result.x
+#print np.exp(result.x)
+print np.exp(result)
+#plot best combination of thetas according to the learning procedure
+#best = (np.exp(result.x[0]), np.exp(result.x[1]), np.exp(result.x[2]), np.exp(result.x[3]))
+#best = (np.exp(result[0]), np.exp(result[1]), np.exp(result[2]), np.exp(result[3]))
+#best = (np.exp(result[0]), np.exp(result[1]), 0, 0)
+best = np.exp(result)
+#best = (np.exp(result.x[0]), np.exp(result.x[1]), np.exp(result.x[2]), np.exp(result.x[3]))
+plt.figure("results according to the learned hyperparameters")
+mu_test, var_test = gp_predictive_distribution(x_train, x_test, t_train, beta, best)
+log_like = gp_log_likelihood(x_train, t_train, best)
+print log_like
+label = 'learned'
+gp_plot(x_test, y_test, mu_test, var_test, x_train, t_train, best, beta, log_like, label)
+plt.legend(loc = 2, prop = {'size': 6})
+plt.xlim(-1,1)
+plt.ylim(-2,2)
 plt.show()
